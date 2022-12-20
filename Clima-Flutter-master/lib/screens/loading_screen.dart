84c40752f887fcf9ac2b53a8527b17be1277c911
Sanika Chandode <sanu30chandode@gmail.com>
2022-12-21 00:06:49 +0530

    Weather app

---
+++ Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('This code of line is triggered');
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http
+        .get('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold(
+        // body: Center(
+        //   child: RaisedButton(
+        //     onPressed: () {
+        //       //Get the current location
+        //       getLocation();
+        //     },
+        //     child: Text('Get Location'),
+        //   ),
+        // ),
+        );
+  }
+}
